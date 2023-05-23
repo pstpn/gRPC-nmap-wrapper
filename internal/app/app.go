c60bc862a5c40ae5289a1d3f53430b33b0e8cfe2
31steets31 <stepaha78@gmail.com>
2023-05-23 22:19:46 +0300

    App: [*] adding reflection

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,6 +3,7 @@ package app
 import (
 	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"net"
 
 	"github.com/gRPC-nmap-wrapper/config"
@@ -14,17 +15,17 @@ import (
 func Run(cfg *config.Config, lg logger.Interface) {
 
 	s := grpc.NewServer()
-	srv := &server.GRPCServer{}
-	api.RegisterNetVulnServiceServer(s, srv)
+	api.RegisterNetVulnServiceServer(s, server.NewServer(lg))
+	reflection.Register(s)
 
 	address := fmt.Sprintf("%s:%d", cfg.Server.Host, cfg.Server.Port)
 	l, err := net.Listen(cfg.Server.Network, address)
 	if err != nil {
-		lg.Error(fmt.Sprintf("app - Run - net.Listen: %e", err))
+		lg.Fatal(fmt.Sprintf("app - Run - net.Listen: %v", err.Error()))
 	}
 	lg.Info(fmt.Sprintf("Server running on %s at %s", cfg.Server.Network, address))
 
 	if err = s.Serve(l); err != nil {
-		lg.Error(fmt.Sprintf("app - Run - Server.Serve: %e", err))
+		lg.Fatal(fmt.Sprintf("app - Run - Server.Serve: %v", err.Error()))
 	}
 }
